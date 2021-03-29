6fb26bb9e08d3b45a6c3a4713a69555d58de49fa
Nick Gerancher <nick@nicksdesk.com>
2021-03-28 21:53:01 -0400

    fixes

--- src/api.ts
+++ src/api.ts
@@ -9,38 +9,35 @@ import {
 export class Thingine {
     private doc!: string;
     private selector!: Element;
+    private selectorID!: string;
     private state!: ThingineState;
 
     private interpolator = new Interpolator();
     private events = new EventSub();
 
     constructor(selector: string = '#main', state: ThingineState) {
-        const selected = document.querySelector(selector);
+        const selected = document.body.querySelector(selector);
         if (!selected || selected.tagName.toLowerCase() !== 'div') {
             this.err(`Unable to render using selector: ${this.selector}`);
             return;
         }
 
-        selected.childNodes.forEach((n, i) => {
+        selected.querySelectorAll('*').forEach((n, i) => {
             if (n instanceof Element) {
                 const find = (n as Element);
                 
                 if (!find.hasAttribute('thingineid')) {
-                    (selected.childNodes[i] as Element).setAttribute('thingineid', this.genID());
+                    n.setAttribute('thingineid', this.interpolator.genID());
                 }
             }
         });
 
         this.doc = selected.innerHTML;
         this.selector = selected;
+        this.selectorID = selector;
         this.state = state;
 
         this.events.setState = this.state;
-    }
-
-    private genID(): string {
-        const id = (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase();
-        return id;
     }
 
     private err(err: any) {
@@ -48,14 +45,90 @@ export class Thingine {
         alert(err);
     }
 
-    private rerender(defs: StateDef<any>, state: StateMethod<any, any>, renderer?: string) {
+    private rerender(store: ThingineState, renderer?: string) {
         if (renderer) {
             this.selector.innerHTML = renderer;
         }
 
-        this.interpolator.findAndReplace(this.selector, defs);
-        this.interpolator.registerActions(this.selector, state);
-        this.interpolator.registerLogic(this.selector, defs, this.events);
+        this.interpolator.findAndReplace(this.selector, store.defs);
+        this.interpolator.registerActions(this.selector, store.methods);
+        this.interpolator.registerLogic(this.selector, store, this.events);
+    }
+
+    private rerenderExclusive(store: ThingineState, renderer?: string, ignoreID?: string) {
+        if (renderer && ignoreID) {
+            const newDom = new DOMParser().parseFromString(renderer, 'text/html').querySelector('body');
+            newDom.childNodes.forEach((newNode, n) => {
+                this.selector.childNodes.forEach((oldNode, o) => {
+                    if (oldNode instanceof Element && newNode instanceof Element) {
+                        const oldElem = (oldNode as Element);
+                        const newElem = (newNode as Element);
+    
+                        if (oldElem.hasAttribute('thingineid') && newElem.hasAttribute('thingineid')) {
+                            const oldElemID = oldElem.getAttribute('thingineid');
+                            const newElemID = newElem.getAttribute('thingineid');
+    
+                            if (oldElemID === newElemID && newElemID !== ignoreID) {
+                                (this.selector.childNodes[o] as Element).innerHTML = (newDom.childNodes[n] as Element).innerHTML;
+                            }
+                        }
+                    }
+                });
+            });
+        } else {
+            this.interpolator.registerLogic(this.selector, store, this.events);
+        }
+
+        this.interpolator.registerActions(this.selector, store.methods);
+        this.interpolator.findAndReplace(this.selector, store.defs);
+    }
+
+    private renderFor(store: ThingineState, forID: string, elems: Element[], baseRemoval?: string, renderer?: string) {
+        if (renderer) {
+            const newDom = new DOMParser().parseFromString(renderer, 'text/html').querySelector('body');
+            newDom.querySelectorAll('*').forEach((newNode, n) => {
+                this.selector.querySelectorAll('*').forEach((oldNode, o) => {
+                    if (oldNode instanceof Element && newNode instanceof Element) {
+                        const newElem = (newNode as Element);
+                        const oldElem = (oldNode as Element);
+    
+                        if (newElem.hasAttribute('thingineid') && oldElem.hasAttribute('thingineid')) {
+                            const newElemID = newElem.getAttribute('thingineid');
+                            const oldElemID = oldElem.getAttribute('thingineid');
+
+                            if (oldElemID === forID) {
+                                elems.forEach((el) => {
+                                    if (oldElem instanceof Element) {
+                                        (oldElem as Element).appendChild(el);
+                                    }
+                                });
+                            }
+    
+                            if (oldElemID === newElemID) {
+                                if (this.selector.querySelectorAll('*')[o] && newDom.querySelectorAll('*')[n]) {
+                                    (this.selector.querySelectorAll('*')[o] as Element).innerHTML = (newDom.querySelectorAll('*')[n] as Element).innerHTML;
+                                }
+                            }
+
+                            if (baseRemoval) {
+                                if (newElemID === baseRemoval) {
+                                    newElem.remove();
+                                }
+
+                                if (oldElemID === baseRemoval) {
+                                    oldElem.remove();
+                                }
+                            }
+                        }
+                    }
+                });
+            });
+        } else {
+            this.interpolator.registerLogic(this.selector, store, this.events);
+        }
+
+        this.interpolator.registerActions(this.selector, store.methods);
+        this.interpolator.findAndReplace(this.selector, store.defs);
     }
 
     public get publisher(): EventSub {
@@ -63,36 +136,63 @@ export class Thingine {
     }
 
     public render() {
-        const oldStore = {
-            ...this.state.defs
+        const oldState = {
+            defs: {
+                ...this.state.defs,
+            },
+            methods: {
+                ...this.state.methods,
+            },
         };
-
-        this.rerender(oldStore, this.state.methods);
 
         Object.keys(this.state.defs).forEach((key) => {
             Object.defineProperty(this.state.defs, key, {
                 get: () => {
-                    return oldStore[key];
+                    return oldState.defs[key];
                 },
                 set: (val) => {
                     this.events.emit(`@update:${key}`, {
-                        old_value: oldStore[key],
+                        old_value: oldState.defs[key],
                         new_value: val, 
                     });
 
-                    oldStore[key] = val;
-                    this.rerender(oldStore, this.state.methods, this.doc);
+                    oldState.defs[key] = val;
+                    this.rerender(oldState, this.doc);
                 }
             });
 
             this.events.subscribe(`@set:${key}`, (_, val) => {
                 this.events.emit(`@update:${key}`, {
-                    old_value: oldStore[key],
-                    new_value: val.value, 
+                    old_value: oldState.defs[key],
+                    new_value: val.value,
                 });
 
-                oldStore[key] = val.value;
+                oldState.defs[key] = val.value;
+                this.rerenderExclusive(oldState, this.doc, val.id);
             });
+
+            if (Array.isArray(oldState.defs[key])) {
+                this.events.subscribe(`@set_for:${key}`, (_, val) => {
+                    this.renderFor(oldState, val.id, val.value.elements, val.value.base, this.doc);
+                    this.interpolator.registerActions(this.selector, oldState.methods);
+                });
+
+                const self = this;
+                const oldPush = oldState.defs[key].push;
+                oldState.defs[key].push = function() {
+                    for (let i = 0; i < arguments.length; i++) {
+                        oldState.defs[key] = [
+                            ...oldState.defs[key],
+                            ...arguments,
+                        ];
+                        oldPush.apply(this, arguments);
+                    }
+
+                    self.rerender(oldState, this.doc);
+                }  
+            }
         });
+
+        this.rerender(oldState);
     }
 }
--- src/interpolation.ts
+++ src/interpolation.ts
@@ -24,12 +24,33 @@ export class Interpolator {
         return checker.test(check);
     }
 
+    private replaceInterpolatedAttr(elem: Element, key: string, replacement: any) {
+        elem.getAttributeNames().forEach((childAttr) => {
+            const attr = elem.getAttribute(childAttr);
+            if (this.isInterpolatedValue(key, attr)) {
+                if (typeof replacement === 'object') {
+                    const flat = this.flattenObject(replacement, key);
+                    Object.keys(flat).forEach((k) => {
+                        const newKey = k.replace('.', '\\.');
+                        const checker = new RegExp(this.interCheck(newKey.trim()), 'g');
+                        elem.setAttribute(childAttr, attr.replace(checker, flat[k]));
+                    });
+                } else {
+                    const checker = new RegExp(this.interCheck(key.trim()), 'g');
+                    elem.setAttribute(childAttr, attr.replace(checker, replacement));
+                }
+            }
+        });
+
+        return elem;
+    }
+
     private replaceInterpolatedValue(key: string, check: string, replacement: any): string {
         let toReplace = check;
 
         if (typeof replacement === 'object') {
             const flat = this.flattenObject(replacement, key);
-            Object.keys(flat).forEach((k, _) => {
+            Object.keys(flat).forEach((k) => {
                 if (this.isInterpolatedValue(k, check)) {
                     const newKey = k.replace('.', '\\.');
                     const checker = new RegExp(this.interCheck(newKey.trim()), 'g');
@@ -46,63 +67,139 @@ export class Interpolator {
         return toReplace;
     }
 
+    public genID(): string {
+        const id = (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase();
+        return id;
+    }
+
     public findAndReplace(elem: Element, defs: StateDef<any>) {
-        Object.keys(defs).forEach((key, _) => {
+        Object.keys(defs).forEach((key) => {
             elem.childNodes.forEach((node, i) => {
-                const replacer = this.replaceInterpolatedValue(key, node.textContent, defs[key]);
-                elem.childNodes[i].textContent = replacer;
+                if (node instanceof Element) {
+                    const el = (node as Element);
+                    const replacer = this.replaceInterpolatedValue(key, el.innerHTML, defs[key]);
+                    (elem.childNodes[i] as Element).innerHTML = replacer;
+                } else {
+                    const replacer = this.replaceInterpolatedValue(key, node.textContent, defs[key]);
+                    elem.childNodes[i].textContent = replacer;
+                }
             });
         });
     }
 
     public registerActions(elem: Element, methods: StateMethod<any, any>) {
-        Object.keys(methods).forEach((key, _) => {
-            elem.childNodes.forEach((node, i) => {
-                if (node instanceof Element) {
-                    const n = (elem.childNodes[i] as Element);
-                    n.getAttributeNames().forEach((attr) => {
-                        if (attr.startsWith('@')) {
-                            const action = attr.substring(1);
-                            n.addEventListener(action, methods[n.getAttribute(attr)]);
+        elem.querySelectorAll('*').forEach((node, i) => {
+            if (node instanceof Element) {
+                const n = (elem.querySelectorAll('*')[i] as Element);
+                n.getAttributeNames().forEach((attr) => {
+                    let action!: string;
+
+                    if (attr.startsWith('@')) {
+                        action = attr.substring(1);
+                    } else if(attr.startsWith('thingine-on:')) {
+                        action = attr.substring(12);
+                    }
+
+                    const func = methods[n.getAttribute(attr)];
+                    if (action && func) {
+                        if (n.hasAttribute('args')) {
+                            const args = n.getAttribute('args');
+                            n.addEventListener(action, func.bind(this, args), false);
+                        } else {
+                            n.addEventListener(action, func.bind(this, null), false);
                         }
-                    });
-                }
-            });
+                    }
+                });
+            }
         });
     }
 
-    public registerLogic(elem: Element, defs: StateDef<any>, events: EventSub) {
-        Object.keys(defs).forEach((key, i) => {
-            elem.childNodes.forEach((node, j) => {
-                if (node instanceof Element) {
-                    const n = (elem.childNodes[j] as Element);                 
-                    n.getAttributeNames().forEach((attr) => {
-                        if (attr.startsWith('%')) {
-                            const action = attr.substring(1);
-                            switch (action) {
-                                case 'if':
-                                    // TODO: handle ifs
-                                    break;
-                                case 'bind':
-                                    const bind = n.getAttribute(attr);
-                                    const input = (n as HTMLInputElement & {
-                                        value: any,
-                                    });
+    public registerLogic(elem: Element, store: ThingineState, events: EventSub) {
+        elem.querySelectorAll('*').forEach((node, j) => {
+            if (node instanceof Element) {
+                const n = (node as Element);             
+                n.getAttributeNames().forEach((attr) => {
+                    if (attr.startsWith('%') || attr.startsWith('thingine-logic:')) {
+                        let action!: string;
 
-                                    input.addEventListener('keyup', (evt) => {
-                                        events.emit(`@set:${bind}`, {
-                                            id: input.getAttribute('thingineid'),
-                                            value: input.value,
-                                        });
-                                    });
-                                    break;
-                                default:
-                                    break;
-                            }
+                        if (attr.startsWith('%')) {
+                            action = attr.substring(1);
+                        } else if(attr.startsWith('thingine-logic:')) {
+                            action = attr.substring(16);
                         }
-                    });
-                }
-            });
+
+                        switch (action) {
+                            case 'if':
+                                // TODO: handle ifs
+                                break;
+                            case 'for':
+                                const iterator = n.getAttribute('%for')
+                                const perIteration = n.getAttribute('%as');
+
+                                if (store.defs[iterator] && store.defs[iterator] !== null) {
+                                    const iter = store.defs[iterator];
+                                    let baseRemovalID = '';
+
+                                    if (Array.isArray(iter)) {
+                                        const elems: Element[] = [];
+
+                                        n.childNodes.forEach((child, i) => {
+                                            if (child instanceof Element) {
+                                                if (this.isInterpolatedValue(perIteration, child.innerHTML)) {
+                                                    iter.forEach((val, idx) => {
+                                                        let elem = document.createElement(child.tagName);
+
+                                                        child.getAttributeNames().forEach((childAttr) => {
+                                                            if (childAttr !== 'thingineid') {
+                                                                const setter = childAttr.replace('@', 'thingine-on:').replace('%', 'thingine-logic:');
+                                                                elem.setAttribute(setter, child.getAttribute(childAttr));
+                                                            }
+                                                        });
+
+                                                        elem.setAttribute('thingineid', `${child.getAttribute('thingineid')}-${idx}`);
+                                                        elem.innerHTML = this.replaceInterpolatedValue(perIteration, child.innerHTML, val);
+
+                                                        elem = this.replaceInterpolatedAttr(elem, perIteration, val) as HTMLElement;
+                                                        
+                                                        elems.push(elem);
+                                                    });
+                                                }
+                                            }
+
+                                            if (elem.querySelectorAll('*')[j].childNodes[i] instanceof Element) {
+                                                baseRemovalID = (elem.querySelectorAll('*')[j].childNodes[i] as Element).getAttribute('thingineid');
+                                            }
+                                        });
+
+                                        events.emit(`@set_for:${iterator}`, {
+                                            id: n.getAttribute('thingineid'),
+                                            value: {
+                                                elements: elems,
+                                                base: baseRemovalID,
+                                            },
+                                        });
+                                    }
+                                }
+                                break;
+                            case 'bind':
+                                const bind = n.getAttribute(attr);
+                                const input = (n as HTMLInputElement & {
+                                    value: any,
+                                });
+
+                                input.addEventListener('keyup', (evt) => {
+                                    events.emit(`@set:${bind}`, {
+                                        id: input.getAttribute('thingineid'),
+                                        value: input.value,
+                                    });
+                                });
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                });
+            }
         });
     }
 }
